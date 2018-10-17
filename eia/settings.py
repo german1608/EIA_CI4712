2b6fd5e0fd9718c27c6ac48bd0c2ab9b0683a32b
German Robayo Paz <germanrobayo33@gmail.com>
2018-10-16 23:34:47 -0400

    anade uri de heroku a ALLOWED_HOSTS

--- eia/settings.py
+++ eia/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'l(5-p^c*9er-d!mepb*p=iim*u3(48gz9+pp43k6y_%94a40!9'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['eia-ci4712.herokuapp.com']
 
 
 # Application definition
