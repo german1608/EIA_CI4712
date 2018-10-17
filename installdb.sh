26a589fca60234aa1f6f04bb9a2cdfa1bc3f520b
German Robayo Paz <germanrobayo33@gmail.com>
2018-10-16 22:38:47 -0400

    [skip travis] aniade comando para crear base de datos

---
+++ installdb.sh
@@ -0,0 +1,11 @@
+sudo -u postgres dropdb eia >> /dev/null
+sudo -u postgres dropuser eia >> /dev/null
+sudo -u postgres createuser eia
+sudo -u postgres createdb eia
+sudo -u postgres psql << EOF
+alter user eia with encrypted password 'eiae123';
+alter role eia set client_encoding to 'utf8';
+alter role eia set default_transaction_isolation to 'read committed';
+alter role eia set timezone to 'utc';
+grant all privileges on database eia to eia ;\q
+EOF
