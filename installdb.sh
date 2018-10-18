e6b56e3f4e46a81902662997c9db8bb49f694bd5
German Robayo Paz <germanrobayo33@gmail.com>
2018-10-18 00:26:18 -0400

    otorga privilegios para crear bases de datos a usuario local

--- installdb.sh
+++ installdb.sh
@@ -7,5 +7,6 @@ alter user eia with encrypted password 'eiae123';
 alter role eia set client_encoding to 'utf8';
 alter role eia set default_transaction_isolation to 'read committed';
 alter role eia set timezone to 'utc';
+alter role eia createdb;
 grant all privileges on database eia to eia ;\q
 EOF
