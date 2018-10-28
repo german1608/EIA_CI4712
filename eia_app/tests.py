dccdcc8c782f14b519842933988893a6a817ebd1
manuelinho96 <mr.1311223@gmail.com>
2018-10-28 11:11:54 -0400

    Agregados test cases para la tabla de organizacion

--- eia_app/tests.py
+++ eia_app/tests.py
@@ -1,4 +1,102 @@
 '''Test para el crud del consultor '''
-from django.test import TestCase #pylint: disable=unused-import
+from django.test import TestCase
+from .forms import *  # pylint: disable=wildcard-import, unused-wildcard-import
+from .models import *  # pylint: disable=wildcard-import, unused-wildcard-import
 
 # Create your tests here.
+
+
+class OrganizacionTestCase(TestCase):
+    ''' Pruebas para la tabla de organizacion '''
+
+    def setUp(self):
+        '''Se crean instancias de organizaciones para realizar pruebas'''
+        form_data = {
+            'razon_social': "natural",
+            'nombre': "Nombre de prueba",
+            'rif': "V-25872062-4",
+            'direccion': "Calle ecuador",
+            'nombre_representante_legal': "manuel",
+            'apellido_representante_legal': "Rodriguez",
+            'cedula_representante_legal': "V25872062",
+            'telefono': "04141633960",
+            'email': "prueba@gmail.ve"}
+        form_organizacion = OrganizacionCreateForm(data=form_data)
+        form_organizacion.save()
+        form_data['rif'] = "V-25872062-9"
+        form_organizacion = OrganizacionCreateForm(data=form_data)
+        form_organizacion.save()
+
+    def test_organizacion_crear(self):
+        '''Prueba para crear una instancia de una organizacion'''
+        form_data = {
+            'razon_social': "natural",
+            'nombre': "Nombre de prueba",
+            'rif': "V-25872062-5",
+            'direccion': "Calle ecuador",
+            'nombre_representante_legal': "manuel",
+            'apellido_representante_legal': "Rodriguez",
+            'cedula_representante_legal': "V25872062",
+            'telefono': "04141633960",
+            'email': "prueba@gmail.ve"}
+        form_organizacion = OrganizacionCreateForm(data=form_data)
+        form_organizacion.save()
+        # pylint: disable=no-member
+        organizacion = Organizacion.objects.get(
+            rif="V-25872062-5")
+        self.assertEqual(organizacion.nombre, "Nombre de prueba")
+
+    def test_organizacion_sin_rif(self):
+        '''Prueba para crear una instancia de una organizacion sin rif'''
+        form_data = {
+            'razon_social': "natural",
+            'nombre': "Nombre de prueba",
+            'rif': "",
+            'direccion': "Calle ecuador",
+            'nombre_representante_legal': "manuel",
+            'apellido_representante_legal': "Rodriguez",
+            'cedula_representante_legal': "V25872062",
+            'telefono': "04141633960",
+            'email': "prueba@gmail.ve"}
+        form_organizacion = OrganizacionCreateForm(data=form_data)
+        self.assertFalse(form_organizacion.is_valid())
+
+    def test_organizacion_sin_nombre(self):
+        '''Prueba para crear una instancia de una organizacion sin nombre'''
+        form_data = {
+            'razon_social': "natural",
+            'nombre': "",
+            'rif': "V-25872062-4",
+            'direccion': "Calle ecuador",
+            'nombre_representante_legal': "manuel",
+            'apellido_representante_legal': "Rodriguez",
+            'cedula_representante_legal': "V25872062",
+            'telefono': "04141633960",
+            'email': "prueba@gmail.ve"}
+        form_organizacion = OrganizacionCreateForm(data=form_data)
+        self.assertFalse(form_organizacion.is_valid())
+
+    def test_organizacion_editar_nombre(self):
+        '''Prueba para editar el nombre de una organizacion'''
+        # pylint: disable=no-member
+        organizacion = Organizacion.objects.get(
+            rif="V-25872062-4")
+        organizacion.nombre = "Nombre de prueba2"
+        organizacion.save()
+        # pylint: disable=no-member
+        organizacion = Organizacion.objects.get(
+            rif="V-25872062-4")
+        self.assertEqual(organizacion.nombre, "Nombre de prueba2")
+
+    def test_organizacion_editar_rif_existente(self):
+        '''Prueba para editar el rif de una organizacion. Se intenta
+        ingresar un rif que ya esta registrado'''
+        # pylint: disable=no-member
+        organizacion = Organizacion.objects.get(
+            rif="V-25872062-4")
+        organizacion.rif = "V-25872062-9"
+        try:
+            organizacion.save()
+            self.fail("Se guardo una instancia de un rif que ya existe")
+        except BaseException:
+            pass
