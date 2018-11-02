9f70f41564da4c02514e7831b8f25412745212a5
German Robayo Paz <germanrobayo33@gmail.com>
2018-11-01 22:11:55 -0400

    arregla problema con static fiels

--- eia/urls.py
+++ eia/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from django.views.generic import TemplateView
 
 urlpatterns = [ #pylint: disable=invalid-name
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='index.html'), name='index'),
     path('users/', include("users.urls")),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
