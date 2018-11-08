ba2adce2f13e1db36c7969b36a8cf396744a72f5
German Robayo Paz <germanrobayo33@gmail.com>
2018-11-07 22:18:33 -0400

    anade LOGIN_URL en settings.py

--- eia/settings.py
+++ eia/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 import dj_database_url
+from django.urls import reverse_lazy
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -42,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users',
+    'dashboard'
 ]
 
 MIDDLEWARE = [
@@ -130,3 +132,5 @@ STATICFILES_DIRS = [
 STATIC_ROOT = os.path.join(BASE_DIR, '.staticfiles')
 
 AUTH_USER_MODEL = 'users.Usuario'
+
+LOGIN_URL = reverse_lazy('login')
