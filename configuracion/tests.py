f5e7633f2da5c333d9f6c83d9af3d1a026c415b8
German Robayo Paz <germanrobayo33@gmail.com>
2018-11-12 14:48:45 -0400

    quita el uso de time

--- configuracion/tests.py
+++ configuracion/tests.py
@@ -2,7 +2,6 @@
 Pruebas Unitarias
 """
 
-import time
 from django.test import Client
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from django.test import TestCase
@@ -222,348 +221,230 @@ class PruebaFormularioEstudio(StaticLiveServerTestCase):
         """
 
         self.browser.get('%s%s' % (self.live_server_url, '/configuracion/index/'))
-        time.sleep(4)
 
         # Volvemos a agregar otro elemento pero ahora de tipo Biologico
         self.browser.find_element_by_css_selector('.btn').click() # Hacemos click en agregar
-        time.sleep(5)
         nombre = "Impacto F"
         self.browser.find_element_by_name('nombre').send_keys(nombre) #agregamos el nombre
-        time.sleep(2)
         select_tipo = Select(self.browser.find_element_by_name('tipo'))
-        time.sleep(2)
         select_tipo.select_by_visible_text('Fisico')
-        time.sleep(2)
         self.browser.execute_script("window.scrollTo(0, 720)") #movemos el scroll un poco
-        time.sleep(2)
         #agregamos la ponderacion de la intensidad
         self.browser.find_element_by_name('pondIntensidad').send_keys(20)
-        time.sleep(2)
         #agregamos la ponderacion de la extension
         self.browser.find_element_by_name('pondExtension').send_keys(20)
-        time.sleep(2)
         #agregamos la ponderacion de la duracion
         self.browser.find_element_by_name('pondDuracion').send_keys(30)
-        time.sleep(2)
         #agregamos la ponderacion de la reversibilidad
         self.browser.find_element_by_name('pondReversibilidad').send_keys(10)
-        time.sleep(2)
         #agregamos la ponderacion de la probabilidad
         self.browser.find_element_by_name('pondProbabilidad').send_keys(20)
-        time.sleep(2)
         # Hacemos click en agregar
         self.browser.find_element_by_name('editar').click()
-        time.sleep(2)
         #para las alertas del navegador
         confirmacion = self.browser.switch_to.alert
-        time.sleep(2)
         confirmacion.accept()
         self.browser.get('%s%s' % (self.live_server_url, '/configuracion/index/'))
-        time.sleep(4)
 
         # Volvemos a agregar otro elemento pero ahora de tipo Biologico
         # Hacemos click en agregar
         self.browser.find_element_by_css_selector('.btn').click()
-        time.sleep(5)
         nombre = "Impacto B"
         #agregamos el nombre
         self.browser.find_element_by_name('nombre').send_keys(nombre)
-        time.sleep(2)
         select_tipo = Select(self.browser.find_element_by_name('tipo'))
-        time.sleep(2)
         # select_tipo.select_by_visible_text('Biologico')
-        # time.sleep(2)
         #movemos el scroll un poco
         self.browser.execute_script("window.scrollTo(0, 720)")
-        time.sleep(2)
         #agregamos la ponderacion de la intensidad
         self.browser.find_element_by_name('pondIntensidad').send_keys(20)
-        time.sleep(2)
         #agregamos la ponderacion de la extension
         self.browser.find_element_by_name('pondExtension').send_keys(20)
-        time.sleep(2)
         #agregamos la ponderacion de la duracion
         self.browser.find_element_by_name('pondDuracion').send_keys(30)
-        time.sleep(2)
         #agregamos la ponderacion de la reversibilidad
         self.browser.find_element_by_name('pondReversibilidad').send_keys(10)
-        time.sleep(2)
 
         # agregamos la ponderacion de la probabilidad
         self.browser.find_element_by_name('pondProbabilidad').send_keys(20)
-        time.sleep(2)
         self.browser.find_element_by_name('editar').click() # Hacemos click en agregar
-        time.sleep(2)
         confirmacion = self.browser.switch_to.alert #para las alertas del navegador
-        time.sleep(2)
         confirmacion.accept()
         self.browser.get('%s%s' % (self.live_server_url, '/configuracion/index/'))
-        time.sleep(4)
 
         # Volvemos a agregar otro elemento pero ahora de tipo Socio-Cultural
         self.browser.find_element_by_css_selector('.btn').click() # Hacemos click en agregar
-        time.sleep(5)
         nombre = "Impacto SC"
         self.browser.find_element_by_name('nombre').send_keys(nombre) #agregamos el nombre
-        time.sleep(2)
         select_tipo = Select(self.browser.find_element_by_name('tipo'))
-        time.sleep(2)
         # select_tipo.select_by_visible_text('Socio-Cultural')
-        # time.sleep(2)
         #movemos el scroll un poco
         self.browser.execute_script("window.scrollTo(0, 720)")
-        time.sleep(2)
         #agregamos la ponderacion de la intensidad
         self.browser.find_element_by_name('pondIntensidad').send_keys(20)
-        time.sleep(2)
         #agregamos la ponderacion de la extension
         self.browser.find_element_by_name('pondExtension').send_keys(20)
-        time.sleep(2)
         #agregamos la ponderacion de la duracion
         self.browser.find_element_by_name('pondDuracion').send_keys(30)
-        time.sleep(2)
         #agregamos la ponderacion de la reversibilidad
         self.browser.find_element_by_name('pondReversibilidad').send_keys(10)
-        time.sleep(2)
         #agregamos la ponderacion de la probabilidad
         self.browser.find_element_by_name('pondProbabilidad').send_keys(20)
-        time.sleep(2)
         # Hacemos click en agregar
         self.browser.find_element_by_name('editar').click()
-        time.sleep(2)
         #para las alertas del navegador
         confirmacion = self.browser.switch_to.alert
-        time.sleep(2)
         confirmacion.accept()
         self.browser.get('%s%s' % (self.live_server_url, '/configuracion/index/'))
-        time.sleep(4)
 
         #Intentamos agregar un impacto que ya se encuentra registrado
         self.browser.find_element_by_css_selector('.btn').click() # Hacemos click en agregar
-        time.sleep(3)
         #agregamos el nombre Impacto SC
         self.browser.find_element_by_name('nombre').send_keys(nombre)
-        time.sleep(2)
         self.browser.execute_script("window.scrollTo(0, 720)") # movemos el scroll un poco
-        time.sleep(4)
 
         # agregamos la ponderacion de la intensidad
         self.browser.find_element_by_name('pondIntensidad').send_keys(20)
-        time.sleep(2)
 
         # agregamos la ponderacion de la extension
         self.browser.find_element_by_name('pondExtension').send_keys(20)
-        time.sleep(2)
 
         # agregamos la ponderacion de la duracion
         self.browser.find_element_by_name('pondDuracion').send_keys(30)
-        time.sleep(2)
 
         # agregamos la ponderacion de la reversibilidad
         self.browser.find_element_by_name('pondReversibilidad').send_keys(10)
-        time.sleep(2)
 
         # agregamos la ponderacion de la probabilidad
         self.browser.find_element_by_name('pondProbabilidad').send_keys(20)
 
 
-        time.sleep(2)
         self.browser.find_element_by_name('editar').click() # Hacemos click en agregar
-        time.sleep(2)
         confirmacion = self.browser.switch_to.alert #para las alertas del navegador
-        time.sleep(2)
         confirmacion.accept()
-        time.sleep(4)
         self.browser.find_element_by_name('nombre').clear()
-        time.sleep(2)
 
         # agregamos el nombre no repetido
         self.browser.find_element_by_name('nombre').send_keys('Impacto 5')
-        time.sleep(2)
         self.browser.execute_script("window.scrollTo(0, 720)") #movemos el scroll un poco
-        time.sleep(4)
         self.browser.find_element_by_name('editar').click() # Hacemos click en agregar
-        time.sleep(4)
         confirmacion.accept()
         self.browser.get('%s%s' % (self.live_server_url, '/configuracion/index/'))
-        time.sleep(4)
 
         self.browser.get('%s%s' % (self.live_server_url, '/configuracion/index/'))
-        time.sleep(5)
 
         #Visualizando los datos del impacto SC y modificandolos
         consulta = Estudio.objects.get(nombre=nombre)
-        time.sleep(2)
         self.browser.find_element_by_name(str(consulta.id)).click() # Hacemos click para consultar
-        time.sleep(5)
         self.browser.execute_script("window.scrollTo(0, 720)") #movemos el scroll un poco
-        time.sleep(10)
         self.browser.execute_script("window.scrollTo(0, 0)") #movemos el scroll un poco
-        time.sleep(2)
         self.browser.find_element_by_name('nombre').clear()
-        time.sleep(2)
 
         #agregamos el nombre
         self.browser.find_element_by_name('nombre').send_keys("Este es un nuevo nombre")
-        time.sleep(3)
         self.browser.execute_script("window.scrollTo(0, 1080)") #movemos el scroll un poco
-        time.sleep(5)
         self.browser.find_element_by_name('pondExtension').clear()
-        time.sleep(2)
 
         #agregamos la ponderacion de la extension
         self.browser.find_element_by_name('pondExtension').send_keys(0)
-        time.sleep(2)
         self.browser.find_element_by_name('pondDuracion').clear()
-        time.sleep(2)
 
         #agregamos la ponderacion de la duracion
         self.browser.find_element_by_name('pondDuracion').send_keys(20)
-        time.sleep(2)
         self.browser.find_element_by_name('pondReversibilidad').clear()
-        time.sleep(2)
 
         #agregamos la ponderacion de la reversibilidad
         self.browser.find_element_by_name('pondReversibilidad').send_keys(40)
-        time.sleep(2)
         self.browser.find_element_by_name('editar').click() # Hacemos click en agregar
-        time.sleep(4)
         confirmacion = self.browser.switch_to.alert #para las alertas del navegador
-        time.sleep(2)
         confirmacion.accept()
         self.browser.get('%s%s' % (self.live_server_url, '/configuracion/index/'))
-        time.sleep(2)
         self.browser.find_element_by_name(str(consulta.id)).click() # Hacemos click para consultar
-        time.sleep(5)
         self.browser.execute_script("window.scrollTo(0, 720)") #movemos el scroll un poco
-        time.sleep(10)
         self.browser.execute_script("window.scrollTo(0, 0)") #movemos el scroll un poco
-        time.sleep(2)
 
         self.browser.get('%s%s' % (self.live_server_url, '/configuracion/index/'))
-        time.sleep(4)
 
         #Consultando los datos y Eliminando el impacto F cambiado
         nombre = "Impacto F"
         consulta = Estudio.objects.get(nombre=nombre)
         self.browser.find_element_by_name(str(consulta.id)).click() # Hacemos click para consultar
-        time.sleep(3)
         self.browser.execute_script("window.scrollTo(0, 1080)") #movemos el scroll un poco
-        time.sleep(4)
         self.browser.find_element_by_name('eliminar').click() # Hacemos click en agregar
-        time.sleep(4)
         confirmacion = self.browser.switch_to.alert #para las alertas del navegador
-        time.sleep(2)
         confirmacion.accept()
         self.browser.get('%s%s' % (self.live_server_url, '/configuracion/index/'))
-        time.sleep(4)
 
         ### Prueba a bases de calculo ###
         self.browser.get('%s%s' % (self.live_server_url, '/configuracion/modificar_tablas/'))
-        time.sleep(5)
         self.browser.execute_script("window.scrollTo(0, 1080)") #movemos el scroll un poco
-        time.sleep(2)
 
         self.browser.find_element_by_name('valor1').clear()
         confirmacion = self.browser.switch_to.alert #para las alertas del navegador
-        time.sleep(2)
         confirmacion.accept()
         #self.browser.find_element_by_name('valor1').send_keys("9.0")
-        time.sleep(1)
         self.browser.find_element_by_name('valor2').clear()
         confirmacion = self.browser.switch_to.alert #para las alertas del navegador
-        time.sleep(2)
         confirmacion.accept()
         #self.browser.find_element_by_name('valor2').send_keys("6.0")
-        time.sleep(1)
         self.browser.find_element_by_name('valor6').clear()
         confirmacion = self.browser.switch_to.alert #para las alertas del navegador
-        time.sleep(2)
         confirmacion.accept()
         #self.browser.find_element_by_name('valor6').send_keys("6.0")
-        time.sleep(1)
         self.browser.find_element_by_name('valor8').clear()
         confirmacion = self.browser.switch_to.alert #para las alertas del navegador
-        time.sleep(2)
         confirmacion.accept()
         #self.browser.find_element_by_name('valor8').send_keys("1.0")
-        time.sleep(1)
         self.browser.find_element_by_name('valor9').clear()
         confirmacion = self.browser.switch_to.alert #para las alertas del navegador
-        time.sleep(2)
         confirmacion.accept()
         #self.browser.find_element_by_name('valor9').send_keys("4.0")
-        time.sleep(1)
         self.browser.find_element_by_name('valor12').clear()
         confirmacion = self.browser.switch_to.alert #para las alertas del navegador
-        time.sleep(2)
         confirmacion.accept()
         #self.browser.find_element_by_name('valor12').send_keys("0.0")
-        time.sleep(1)
         self.browser.find_element_by_name('valor13').clear()
         confirmacion = self.browser.switch_to.alert #para las alertas del navegador
-        time.sleep(2)
         confirmacion.accept()
         #self.browser.find_element_by_name('valor13').send_keys("9.0")
-        time.sleep(1)
         self.browser.find_element_by_name('valor14').clear()
         confirmacion = self.browser.switch_to.alert #para las alertas del navegador
-        time.sleep(2)
         confirmacion.accept()
         #self.browser.find_element_by_name('valor14').send_keys("6.0")
-        time.sleep(1)
         self.browser.find_element_by_name('valor15').clear()
         confirmacion = self.browser.switch_to.alert #para las alertas del navegador
-        time.sleep(2)
         confirmacion.accept()
         #self.browser.find_element_by_name('valor15').send_keys("4.0")
-        time.sleep(1)
         self.browser.find_element_by_name('valor16').clear()
         confirmacion = self.browser.switch_to.alert #para las alertas del navegador
-        time.sleep(2)
         confirmacion.accept()
         #self.browser.find_element_by_name('valor16').send_keys("1.0")
-        time.sleep(1)
         self.browser.find_element_by_name('valor17').clear()
         confirmacion = self.browser.switch_to.alert #para las alertas del navegador
-        time.sleep(2)
         confirmacion.accept()
         #self.browser.find_element_by_name('valor17').send_keys("1.0")
-        time.sleep(1)
         self.browser.find_element_by_name('valor19').clear()
         confirmacion = self.browser.switch_to.alert #para las alertas del navegador
-        time.sleep(2)
         confirmacion.accept()
         #self.browser.find_element_by_name('valor19').send_keys("8.0")
-        time.sleep(1)
         self.browser.find_element_by_name('valor21').clear()
         confirmacion = self.browser.switch_to.alert #para las alertas del navegador
-        time.sleep(2)
         confirmacion.accept()
         #self.browser.find_element_by_name('valor21').send_keys("9.0")
-        time.sleep(1)
         self.browser.find_element_by_name('valor24').clear()
         confirmacion = self.browser.switch_to.alert #para las alertas del navegador
-        time.sleep(2)
         confirmacion.accept()
         #self.browser.find_element_by_name('valor24').send_keys("0.0")
-        time.sleep(1)
         self.browser.find_element_by_name('valor27').clear()
         confirmacion = self.browser.switch_to.alert #para las alertas del navegador
-        time.sleep(2)
         confirmacion.accept()
         #self.browser.find_element_by_name('valor27').send_keys("2.0")
-        time.sleep(1)
         self.browser.find_element_by_name('valor32').clear()
         confirmacion = self.browser.switch_to.alert #para las alertas del navegador
-        time.sleep(2)
         confirmacion.accept()
         #self.browser.find_element_by_name('valor32').send_keys("1.0")
-        time.sleep(2)
         self.browser.find_element_by_name('submit').click()
-        time.sleep(5)
         self.browser.get('%s%s' % (self.live_server_url, '/configuracion/tablas/'))
-        time.sleep(4)
 
     def tearDown(self):
         """
