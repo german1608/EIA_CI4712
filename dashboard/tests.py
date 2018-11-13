d98b416f4125dc891224b3ffeb8201c56b56ec36
German Robayo Paz <germanrobayo33@gmail.com>
2018-11-12 20:22:10 -0400

    anade prueba para probar el home

--- dashboard/tests.py
+++ dashboard/tests.py
@@ -2,8 +2,10 @@
 Pruebas unitarias del dashboard de EIA
 """
 from django.test import TestCase, Client
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from django.urls import reverse
 from django.conf import settings
+from selenium.webdriver.firefox.webdriver import WebDriver
 from users.models import Usuario
 from .views import DashboardView
 
@@ -48,3 +50,38 @@ class TestViews(TestCase):
         self.assertEqual(response.resolver_match.func.__name__,
                          DashboardView.as_view().__name__)
         self.client.logout()
+
+
+class TestDashboardInterface(StaticLiveServerTestCase):
+    """
+    Suite de pruebas para la interfaz del dashboard
+    """
+    fixtures = ['users-and-groups.json']
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.selenium = WebDriver()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.selenium.quit()
+        super().tearDownClass()
+
+    def setUp(self):
+        """
+        Configuracion necesaria para cada prueba del suite.
+        Crea un usuario para poder probar autenticacion en la plataforma
+        """
+        self.usuario = Usuario.objects.get(username='admin')
+
+    def test_interface_content(self):
+        """ Verificamos que el contenido de la interfaz sea el correcto """
+        # Nos loggeamos
+        self.selenium.get('{}{}'.format(self.live_server_url, reverse('dashboard:index')))
+        self.selenium.find_element_by_id('id_username').send_keys('admin')
+        self.selenium.find_element_by_id('id_password').send_keys('jaja1234')
+        self.selenium.find_element_by_id('id_submit').click()
+
+        # Obtenemos el contenido del elemento del side-menu
+        nombre_usuario_input = self.selenium.find_element_by_id('side-menu-user-name')
+        self.assertEqual(nombre_usuario_input.text, 'Bienvenido %s' % self.usuario.full_name())
