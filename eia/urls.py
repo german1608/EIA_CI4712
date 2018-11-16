602b69a52cf75e9e07d0b29df315f2cf8aa87b95
manuelitod <mr.1311223@gmail.com>
2018-11-15 20:16:48 -0400

    Agregada configuracion para los image field. Agregada las migraciones del modelo descripcionproyecto

--- eia/urls.py
+++ eia/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from eia_app import views
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,3 +28,8 @@ urlpatterns = [
     path('', include('dashboard.urls')),
     path('configuracion/', include("configuracion.urls")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT, }),
+    ]
