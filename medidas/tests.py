104989088848d26be64cfc68d3428b72edef19a8
German Robayo Paz <germanrobayo33@gmail.com>
2018-11-26 14:25:06 -0400

    anade prueba de nomenclatura requerida

--- medidas/tests.py
+++ medidas/tests.py
@@ -2,6 +2,7 @@
 Pruebas unitarias del modulo de medidas
 '''
 from django.test import TestCase
+from django.forms.models import model_to_dict
 from .forms import MedidaForm
 from .models import Medida
 
@@ -12,7 +13,14 @@ class MedidaFormTestCase(TestCase):
 
     def setUp(self):
         self.medida = Medida.objects.get(nombre='Medida 1')
-
+        self.medida_dict = model_to_dict(self.medida)
 
     def test_form_existence(self):
+        ''' Test de existencia de MedidaForm '''
         MedidaForm()
+
+    def test_form_nomenclatura_not_empty(self):
+        ''' Prueba para validar la nomenclatura '''
+        self.medida_dict['nomenclatura'] = ''
+        form = MedidaForm(self.medida_dict)
+        self.assertFalse(form.is_valid(), 'El formulario no debe estar valido sin la nomenclatura')
