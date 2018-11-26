2036c33bd012422e6277b028f5e2acd62e873344
German Robayo Paz <germanrobayo33@gmail.com>
2018-11-26 14:16:52 -0400

    anade test de existencia de medidaform

--- medidas/tests.py
+++ medidas/tests.py
@@ -1,3 +1,18 @@
+'''
+Pruebas unitarias del modulo de medidas
+'''
 from django.test import TestCase
+from .forms import MedidaForm
+from .models import Medida
 
 # Create your tests here.
+class MedidaFormTestCase(TestCase):
+    ''' Pruebas unitarias para el formulario de medida '''
+    fixtures = ['medidas.json']
+
+    def setUp(self):
+        self.medida = Medida.objects.get(nombre='Medida 1')
+
+
+    def test_form_existence(self):
+        MedidaForm()
