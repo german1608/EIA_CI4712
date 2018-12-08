a5b8e912ad1135407b4eb04b99b5cbcb6c05f103
Giulianne17 <gvtr96@gmail.com>
2018-12-08 16:01:22 +0100

    Se añadieron las prueba de las caracteristicas de un medio. - Pruebas de añadir, eliminar, editar y detalles de un medio. - Pruebas de añadir, detalles, editar y eliminar una caraceristica. - Pruebas de añadir, eliminar y editar una subcaracteristica.

--- eia_app/test_selenium.py
+++ eia_app/test_selenium.py
@@ -571,3 +571,217 @@ class TestDescripcionProyecto(SeleniumTestCase):
         self.selenium.find_element_by_css_selector(
             '#borrar' + str(self.descripcion.id)).click()
         self.selenium.find_element_by_xpath("//button[@type='submit']").click()
+
+
+class TestCaracteristicaMedio(SeleniumTestCase):
+    """
+        Clase para probar las caracteristicas de los medios del crud del consultor ambiental.
+    """
+
+    def setUp(self):
+        # Se crea un proyecto para editar y eliminar
+        self.base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        self.image_path = os.path.join(self.base_dir, 'static', 'img', 'germanbb.jpg')
+        self.proyecto = DatosProyecto.objects.create(
+            titulo="prueba de detalles",
+            ubicacion="caracas, venezuela",
+            area="area de prueba, caracas",
+            tipo="prueba selenium",
+            url="www.google.com.ve")
+
+        self.proyecto1 = DatosProyecto.objects.create(
+            titulo="prueba de detalles1",
+            ubicacion="caracas, venezuela1",
+            area="area de prueba, caracas1",
+            tipo="prueba selenium1",
+            url="www.google1.com.ve")
+
+        self.medio1=Medio.objects.create(
+            tipo= "biologico",
+            proyecto= self.proyecto1,
+            descripcion= "descripcion del medio",
+            conclusiones= "conclusiones sobre el medio"
+        )
+
+        self.caracteristica1=CaracteristicaMedio.objects.create(
+            caracteristica = "caracteristica",
+            medio =self.medio1,
+            descripcion ="descripcion"
+        )
+
+        self.subcaracteristica1=SubaracteristicaMedio.objects.create(
+            nombre_sub= 'nombre',
+            caracteristica= self.caracteristica1,
+            atributo= 'atributo',
+            comentario='comentario'
+        )
+
+        # Se inicia sesion en cada prueba de forma automatica
+        self.selenium.get(
+            '{}{}'.format(
+                self.live_server_url,
+                reverse('dashboard:index')))
+        self.selenium.find_element_by_id('id_username').send_keys('admin')
+        self.selenium.find_element_by_id('id_password').send_keys('jaja1234')
+        self.selenium.find_element_by_id('id_submit').click()
+
+    def test_anadir_medio(self):
+        """
+            Test para anadir medio de un proyecto.
+        """
+        self.selenium.get('%s%s' % (self.live_server_url, '/consultor-crud/'))
+        self.selenium.find_element_by_css_selector(
+            '#caracteristicas-medio').click()
+        self.selenium.find_element_by_css_selector('#add').click()
+        tipo = "biologico"
+        self.selenium.find_element_by_name(
+            'tipo').send_keys(tipo)
+        proy = self.proyecto1.titulo
+        self.selenium.find_element_by_name('proyecto').send_keys(proy)
+        descripcion= "Descripcion del medio",
+        self.selenium.find_element_by_name(
+            'descripcion').send_keys(descripcion)
+        conclusiones= "Conclusiones sobre el medio"
+        self.selenium.find_element_by_name(
+            'conclusiones').send_keys(conclusiones)
+
+    def test_ver_detalles_medio(self):
+        """
+            Probar el boton de ver detalles de un medio de un proyecto.
+        """
+        self.selenium.get(
+            '%s%s' %
+            (self.live_server_url,
+             '/consultor-crud/caracteristicas_medio/'))
+        self.selenium.find_element_by_css_selector(
+            '#detalles' + str(self.medio1.id)).click()
+
+    def test_editar_medio(self):
+        """
+            Editar los datos de un medio
+        """
+        self.selenium.get(
+            '%s%s' %
+            (self.live_server_url,
+             '/consultor-crud/caracteristicas_medio/'))
+        self.selenium.find_element_by_css_selector(
+            '#editar' + str(self.medio1.id)).click()
+        descripcion = "Edicion de prueba"
+        self.selenium.find_element_by_name('descripcion').send_keys(descripcion)
+        self.selenium.find_element_by_xpath("//button[@type='submit']").click()
+
+    def test_eliminar_medio(self):
+        """
+            Borrar los datos de un medio de un proyecto
+        """
+        self.selenium.get(
+            '%s%s' %
+            (self.live_server_url,
+             '/consultor-crud/caracteristicas_medio/'))
+        self.selenium.find_element_by_css_selector(
+            '#borrar' + str(self.medio1.id)).click()
+        self.selenium.find_element_by_xpath("//button[@type='submit']").click()
+
+    def test_anadir_caracteristica(self):
+        """
+            Test para anadir caracteristica de medio de un proyecto.
+        """
+        self.selenium.get(
+            '%s%s' %
+            (self.live_server_url,
+             '/consultor-crud/caracteristicas_medio/'))
+        self.selenium.find_element_by_css_selector(
+            '#detalles' + str(self.medio1.id)).click()
+        self.selenium.find_element_by_css_selector('#add').click()
+        caracteristica = "caracteristica"
+        self.selenium.find_element_by_name(
+            'caracteristica').send_keys(caracteristica)
+        proy = self.medio1.id
+        descripcion= "Descripcion"
+        self.selenium.find_element_by_name(
+            'descripcion').send_keys(descripcion)
+
+    def test_ver_detalles_caracteristica(self):
+        """
+            Probar el boton de ver detalles de una caracteristica de medio de un proyecto.
+        """
+        self.selenium.get(
+            '%s%s' %
+            (self.live_server_url,
+             '/consultor-crud/caracteristica_medio/'+str(self.medio1.id) +'/'))
+        self.selenium.find_element_by_css_selector(
+            '#detalles' + str(self.caracteristica1.id)).click()
+
+    def test_editar_caracteristica(self):
+        """
+            Editar las caracteristicas de un medio
+        """
+        self.selenium.get(
+            '%s%s' %
+            (self.live_server_url,
+             '/consultor-crud/caracteristica_medio/'+str(self.medio1.id) +'/' ))
+        self.selenium.find_element_by_css_selector(
+            '#editar' + str(self.caracteristica1.id)).click()
+        descripcion = "Edicion de prueba"
+        self.selenium.find_element_by_name('descripcion').send_keys(descripcion)
+        self.selenium.find_element_by_xpath("//button[@type='submit']").click()
+
+    def test_eliminar_caracteristica(self):
+        """
+            Borrar las caracteristicas de un medio de un proyecto
+        """
+        self.selenium.get(
+            '%s%s' %
+            (self.live_server_url,
+             '/consultor-crud/caracteristica_medio/' +str(self.medio1.id) +'/' ))
+        self.selenium.find_element_by_css_selector(
+            '#borrar' + str(self.caracteristica1.id)).click()
+        self.selenium.find_element_by_xpath("//button[@type='submit']").click()
+
+    def test_anadir_subcaracteristica(self):
+        """
+            Test para anadir subcaracteristica de medio de un proyecto.
+        """
+        self.selenium.get(
+            '%s%s' %
+            (self.live_server_url,
+             '/consultor-crud/caracteristica_medio/' +str(self.medio1.id) +'/' ))
+        self.selenium.find_element_by_css_selector(
+            '#detalles' + str(self.caracteristica1.id)).click()
+        self.selenium.find_element_by_css_selector('#add').click()
+        nombre_sub = "nombre_sub"
+        self.selenium.find_element_by_name(
+            'nombre_sub').send_keys(nombre_sub)
+        caracteristica = self.caracteristica1.id
+        atributo= "atributo"
+        self.selenium.find_element_by_name(
+            'atributo').send_keys(atributo)
+        comentario= "comentario"
+        self.selenium.find_element_by_name(
+            'comentario').send_keys(comentario)
+
+    def test_editar_subcaracteristica(self):
+        """
+            Editar las subcaracteristicas de un medio
+        """
+        self.selenium.get(
+            '%s%s' %
+            (self.live_server_url,
+             '/consultor-crud/caracteristica_medio/detalles/caracteristica/'+str(self.caracteristica1.id) +'/' ))
+        self.selenium.find_element_by_css_selector(
+            '#editar' + str(self.subcaracteristica1.id)).click()
+        atributo = "Edicion de prueba"
+        self.selenium.find_element_by_name('atributo').send_keys(atributo)
+        self.selenium.find_element_by_xpath("//button[@type='submit']").click()
+
+    def test_eliminar_subcaracteristica(self):
+        """
+            Borrar las subcaracteristicas de un medio de un proyecto
+        """
+        self.selenium.get(
+            '%s%s' %
+            (self.live_server_url,
+              '/consultor-crud/caracteristica_medio/detalles/caracteristica/'+str(self.caracteristica1.id) +'/' ))
+        self.selenium.find_element_by_css_selector(
+            '#borrar' + str(self.subcaracteristica1.id)).click()
+        self.selenium.find_element_by_xpath("//button[@type='submit']").click()
