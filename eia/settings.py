5085bc051b87b1ee38c9efc90cd964f5e4c0d97d
Daniel <danielrs9705@gmail.com>
2019-01-02 18:03:36 -0400

    Se elimina una configuracion de whitenoise

--- eia/settings.py
+++ eia/settings.py
@@ -142,7 +142,7 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, '.staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 AUTH_USER_MODEL = 'users.Usuario'
 
